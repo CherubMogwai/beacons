023c05cae148bf01f16dd9fcee1a1145c9403e73
Brent Luehr <brent@nerdcon.com>
2016-09-21 23:30:47 -0700

    Add test for checking server status page

--- test/index.js
+++ test/index.js
@@ -3,7 +3,13 @@
 import test from 'ava';
 import { beacons as config } from 'config';
 import request from 'supertest-as-promised';
+import service from 'server';
 
-test(`stub for future tests`, t => {
-  return t.pass();
+const app = service.listen();
+
+test(`should be able to load status page`, async t => {
+  const res = await request(app).get(`/status`);
+
+  t.is(res.statusCode, 200);
+  t.true(res.body.success);
 });
