cc7735e41eef52802df5bc74858a1462742aad74
Brent Luehr <brent@nerdcon.com>
2016-10-02 16:18:05 -0700

    Location saving, nearby beacon lookup, and boundary schema added

--- models/beacon.js
+++ models/beacon.js
@@ -2,8 +2,53 @@ import Model from './index';
 
 export default class Beacon extends Model {
 
-  constructor() {
+  constructor(id) {
     super();
+    this.id = id;
+  }
+
+  /**
+   * Updates a beacon's location, or inserts it if no record found
+   * @param lat
+   * @param lng
+   * @returns {*}
+   */
+  async updateLocation(lat, lng) {
+
+    console.log('Updated location:', this.id, lat, lng);
+
+    return await Model.query(`
+      WITH update AS (
+        UPDATE beacons 
+        SET location = ll_to_earth($2, $3), last_seen = NOW()
+        WHERE id = $1
+        RETURNING id
+      )
+      INSERT INTO beacons (id, location, last_seen)
+      SELECT $1, ll_to_earth($2, $3), NOW()
+      WHERE NOT EXISTS (SELECT id FROM update)
+    `, [ this.id, lat, lng ]);
+
+  }
+
+  /**
+   * Retrieves a list of the closest beacons, ordered by nearest to farthest
+   * @returns {SQLResultSetRowList}
+   */
+  async getNearby() {
+
+    const results = await Model.query(`
+      SELECT 
+        *,
+        earth_distance(location, (SELECT location FROM beacons WHERE id = $1)) AS distance
+      FROM beacons
+      WHERE id != $1
+      ORDER BY distance ASC
+      LIMIT 5
+    `, [ this.id ]);
+
+    return results.rows;
+
   }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-// import Beacon from 'models/beacon';
+import { beacons as config } from 'config';
+import Beacon from 'models/beacon';
 
 export default function(router) {
 
   router.get('/status', async ctx => {
     ctx.body = { success: true };
+  });
+
+  router.post('/webhook/location', async ctx => {
+
+    const {
+      event,
+      data,
+      coreid: id,
+      published_at: timestamp
+    } = ctx.request.body;
+
+    const [ lat, lng ] = data.split(',');
+
+    const beacon = new Beacon(id);
+    await beacon.updateLocation(lat, lng);
+    const nearbyBeacons = await beacon.getNearby();
+
+    // Default [ r, g, b, intensity ] values
+    let response = config.lightStates.outOfBounds;
+
+    // Nearby beacons found
+    if (Array.isArray(nearbyBeacons) && nearbyBeacons.length) {
+      // Get nearest beacon
+      const nearest = nearbyBeacons[0];
+      const { distance } = nearest;
+
+
+
+    }
+
+    ctx.body = {
+      data: response.join(',')
+    };
+
   });
 
 }
