6c4c9fadda6b6e7e117a497d01b32c9d660cabad
Melany Nugent <melnugent@mac.com>
2019-07-10 11:42:10 -0700

    get janet difference teest

--- models/alice.js
+++ models/alice.js
@@ -15,14 +15,40 @@ export default class Alice {
     this.ladies = beacons
   }
 
-  where_janet(){
-    const janet = this.ladies.find((lady) => lady.name === "Janet");
+  where_janet(){ //difference intebnsity
+    const janet = this.get_janet()
+
     if (!janet){
       return null
     }
+    //
 var diff = this.lat - janet.lat;
 console.log([this.lat, janet.lat, diff]);
 return diff
+  }
+
+
+  get_janet(){ //janet is here
+    const janet = this.ladies.find((lady) => lady.name === "Janet");
+    return janet
+  }
+  reply(){
+
+    var r = Math.random();
+    var i = parseInt(r*255);
+
+
+    if (this.get_janet()) {
+      var diff = this.where_janet()
+      if (diff > 170) {
+          return [...[ 50, i, 100 ], 100]
+      } else if (diff > 20  && diff < 90) {
+        return [...[ 50, i, 100 ], 100]
+      }
+      else (diff < 20){
+        return [...[ 250, 0, 0 ], 100]
+        }
+    }
   }
 
   where_brett(){
@@ -34,22 +60,6 @@ return diff
     var diff = this.lat - brett.lat;
     console.log([this.lat, brett.lat, diff] );
     return diff
-  }
-
-  reply(){
-
-    var r = Math.random();
-    var i = parseInt(r*255);
-
-    if (this.where_janet()) {
-      return [...[ 50, i, 100 ], 100]
-        }
-      else {
-      return [...[ 255, 0, 0 ], 100]
-    }
-
-    //  return [...[ 50, i, 100 ], 100]
-    }
   }
 
 //  reply(){
