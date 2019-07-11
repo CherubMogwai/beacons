9ae4a7ea7f820300ddca93a8d86b8ed1384586ba
Melany Nugent <melnugent@mac.com>
2019-07-11 09:22:52 -0700

    add brett

--- models/alice.js
+++ models/alice.js
@@ -1,56 +1,72 @@
 export default class Alice {
-  constructor(){
-   this.name = "Alice";
-   this.lat = 0;
-   this.lng = 0;
-   this.ladies = [];
+  constructor() {
+    this.name = "Alice";
+    this.lat = 0;
+    this.lng = 0;
+    this.ladies = [];
   }
   set_lat(lat) {
     this.lat = lat;
   }
   set_lng(lng) {
-    this.lng = lng
+    this.lng = lng;
   }
-  set_ladies(beacons){
-    this.ladies = beacons
+  set_ladies(beacons) {
+    this.ladies = beacons;
   }
 
-  where_janet(){ //difference intebnsity
+  where_janet() { // difference intensity
     const janet = this.get_janet();
 
-    if (!janet){
-      return null
+    if (!janet) {
+      return null;
     }
     //
     var diff = this.lat - janet.lat;
     console.log([this.lat, janet.lat, diff]);
-    return diff
+    return diff;
   }
 
+  where_brett() { // difference intensity
+    const brett = this.get_brett();
 
-  get_janet(){ //janet is here
+    if (!brett) {
+      return null;
+    }
+    //
+    var diff = this.lat - brett.lat;
+    console.log([this.lat, brett.lat, diff]);
+    return diff;
+  }
+
+  get_janet() { // janet is here
     const janet = this.ladies.find((lady) => lady.name === "Janet");
-    return janet
+    return janet;
   }
 
-  reply(){
+  get_brett() { // janet is here
+    const brett = this.ladies.find((lady) => lady.name === "Brett");
+    return brett;
+  }
 
-    var r = Math.random();
-    var i = parseInt(r * 255);
+  reply() {
 
+     var r = Math.random();
+     var i = parseInt(r * 255);
 
     if (this.get_janet()) {
+
       var diff = this.where_janet();
 
       if (diff > 170) {
-        return [ ...[ 50, i, 100 ], 100];
+        return [ ...[ 50, 50, 50 ], 1];
       } else if ((diff > 20) && (diff < 90)) {
         return [ ...[ 50, i, 100 ], 100];
       } else if (diff < 20) {
         return [ ...[250, 0, 0 ], 100];
       }
     }
-    return [...[ 50, i, 100 ], 100];
+     return [...[ 50, i, 100 ], 100];
   }
 
   // where_brett(){
@@ -71,4 +87,4 @@ export default class Alice {
 //    intensity = 50 - this.where_janet();
 //    return [...[ 0, 255, 0, intensity ]]
 //  }
-//}
+// }
