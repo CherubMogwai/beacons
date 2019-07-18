19a201a107f0a985aa3432251f440767923a609b
Melany Nugent <melnugent@mac.com>
2019-07-18 10:10:03 -0700

    brett gps

--- models/alice.js
+++ models/alice.js
@@ -15,7 +15,7 @@ export default class Alice {
     this.ladies = beacons;
   }
 
-  where_janet() { // difference intensity
+  where_janet() { // difference between alice and janet to set intensity
     const janet = this.get_janet();
 
     if (!janet) {
@@ -64,9 +64,41 @@ export default class Alice {
       return null;
     }
 
-    var brettDiff = this.lat - brett.lat;
-    console.log([this.lat, brett.lat, brettDiff]);
-    return brettDiff;
+    var R = 6371e3; // metres
+    var φ1 = this.lat * Math.PI / 180;
+    // var φ2 = janet.lat * Math.PI / 180;
+    var φ3 = brett.lat.toRadians();
+
+    // var aΔφ = (this.lat - janet.lat) * Math.PI / 180;
+    // var aΔλ = (janet.lng - this.lng) * Math.PI / 180;
+
+    var bΔφ = (this.lat - brett.lat) * Math.PI / 180;
+    var bΔλ = (brett.lng - this.lng) * Math.PI / 180;
+
+    // var a = Math.sin(aΔφ / 2) * Math.sin(aΔφ / 2) +
+      //  Math.cos(φ1) * Math.cos(φ2) *
+      //  Math.sin(aΔλ / 2) * Math.sin(aΔλ / 2);
+
+    var e = Math.sin(bΔφ / 2) * Math.sin(bΔφ / 2) +
+         Math.cos(φ1) * Math.cos(φ3) *
+         Math.sin(bΔλ / 2) * Math.sin(bΔλ / 2);
+
+  //  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var f = 2 * Math.atan2(Math.sqrt(e), Math.sqrt(1 - e));
+
+    // var d = R * c;
+    var g = R * f;
+    // var h = (d + g / 2);
+
+    console.log([this.lat, brett.lat, g]);
+    return g;
+
+
+
+
+    // var brettDiff = this.lat - brett.lat;
+    // console.log([this.lat, brett.lat, brettDiff]);
+    // return brettDiff;
   }
 
   get_janet() { // janet is here
@@ -88,13 +120,13 @@ export default class Alice {
     var janetDiff = this.where_janet();
 
 
-    if (this.get_janet()) {
+    if (this.get_brett()) {
        // return [ ...[250, 0, 150 ], 100];
-      if (janetDiff < 20) {
+      if (brettDiff < 20) {
         return [ ...[ 250, 0, 0 ], 50];
-      } else if ((janetDiff > 20) && (janetDiff < 100)) {
+      } else if ((brettDiff > 20) && (brettDiff < 100)) {
         return [ ...[ 50, 255, 100 ], 100];
-      } else if (janetDiff > 100) {
+      } else if (brettDiff > 100) {
         return [ ...[250, 255, 255 ], 100];
       }
     // else if (this.get_janet() && (this.get_brett())) {
