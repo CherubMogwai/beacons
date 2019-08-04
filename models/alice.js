6a936046a618bf7b30fd6c39c5a1f6f409f5224b
Melany Nugent <melnugent@mac.com>
2019-08-04 13:14:54 -0700

    update alice

--- models/alice.js
+++ models/alice.js
@@ -1,9 +1,9 @@
 export default class Alice {
-  constructor(){
-   this.name = "Alice";
-   this.lat = 0;
-   this.lng = 0;
-   this.ladies = [];
+  constructor() {
+    this.name = "Alice";
+    this.lat = 0;
+    this.lng = 0;
+    this.ladies = [];
   }
   set_lat(lat) {
     this.lat = lat;
@@ -11,16 +11,138 @@ export default class Alice {
   set_lng(lng) {
     this.lng = lng
   }
-  set_ladies(beacons){
+  set_ladies(beacons) {
     this.ladies = beacons
   }
 
-  reply(){
-    var r = Math.random();
-    var i = parseInt(r*255);
-    return [...[ i, 25, 100 ], 100]
+  where_brett() {
+    const brett = this.get_brett();
+
+    if (!brett) {
+      return null;
+    }
+
+    var R = 6371e3; // metres
+    var φ1 = this.lat * Math.PI / 180;
+    var φ2 = brett.lat * Math.PI / 180;
+
+    var aΔφ = (this.lat - brett.lat) * Math.PI / 180;
+    var aΔλ = (brett.lng - this.lng) * Math.PI / 180;
+
+    var a = Math.sin(aΔφ / 2) * Math.sin(aΔφ / 2) +
+         Math.cos(φ1) * Math.cos(φ2) *
+         Math.sin(aΔλ / 2) * Math.sin(aΔλ / 2);
+
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = (Math.floor(R * c));
+
+    console.log(["Brett =", d, "meters"]);
+    return d;
+  }
+
+where_janet() { // difference intensity
+  const janet = this.get_janet();
+
+  if (!janet) {
+    return null;
+  }
+
+  var R = 6371e3; // metres
+  var φ1 = this.lat * Math.PI / 180;
+     // var φ2 = brett.lat * Math.PI / 180;
+  var φ3 = janet.lat * Math.PI / 180;
+
+  var bΔφ = (this.lat - janet.lat) * Math.PI / 180;
+  var bΔλ = (janet.lng - this.lng) * Math.PI / 180;
+
+  var e = Math.sin(bΔφ / 2) * Math.sin(bΔφ / 2) +
+          Math.cos(φ1) * Math.cos(φ3) *
+          Math.sin(bΔλ / 2) * Math.sin(bΔλ / 2);
+
+  var f = 2 * Math.atan2(Math.sqrt(e), Math.sqrt(1 - e));
+  var g = (Math.floor(R * f));
+
+  console.log(["Janet = ", g, "meters"]);
+  return g;
+}
+
+  where_both() { // difference intensity
+    const janet = this.get_janet();
+    const brett = this.get_brett();
+
+    if ((!janet) || (!brett)) {
+      return null;
+    }
+
+    var R = 6371e3; // metres
+    var φ1 = this.lat * Math.PI / 180;
+    var φ2 = brett.lat * Math.PI / 180;
+    var φ3 = janet.lat * Math.PI / 180;
+
+    var aΔφ = (this.lat - brett.lat) * Math.PI / 180;
+    var aΔλ = (brett.lng - this.lng) * Math.PI / 180;
+
+    var bΔφ = (this.lat - janet.lat) * Math.PI / 180;
+    var bΔλ = (janet.lng - this.lng) * Math.PI / 180;
+
+    var a = Math.sin(aΔφ / 2) * Math.sin(aΔφ / 2) +
+         Math.cos(φ1) * Math.cos(φ2) *
+         Math.sin(aΔλ / 2) * Math.sin(aΔλ / 2);
+
+    var e = Math.sin(bΔφ / 2) * Math.sin(bΔφ / 2) +
+          Math.cos(φ1) * Math.cos(φ3) *
+          Math.sin(bΔλ / 2) * Math.sin(bΔλ / 2);
+
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var f = 2 * Math.atan2(Math.sqrt(e), Math.sqrt(1 - e));
+
+    var d = R * c;
+    var g = R * f;
+    var h = (Math.floor((d + g) / 2));
+
+    console.log(["Both = ", h, "meters"]);
+    // return g;
+    return h;
+  }
+
+  get_brett() { // brett is here
+    const brett = this.ladies.find((lady) => lady.name === "Brett");
+    return brett;
+  }
+
+  get_janet() { // janet is here
+    const janet = this.ladies.find((lady) => lady.name === "Janet");
+    return janet;
+  }
+
+  reply() {
+
+    var k = (this.where_janet() );
+    var s = (this.where_brett() );
+    var t = (this.where_both() );
+
+    if ((s > 100) && (k > 100) && (t > 100)) {
+      console.log([ "No one is here" ]);
+      return [ ...[ 255, 0, 0 ], 1];
+    } else if ((s < 100) && (k > 100) && (t > 100)) {
+      console.log([ "Brett is here" ]);
+      return [ ...[ 255, 255, 0 ], s];
+    } else if ((s > 100) && (k < 100) && (t > 100)) {
+      console.log([ "Janet is here" ]);
+      return [ ...[ 255, 0, 150 ], k];
+    } else if ((s < 100) && (k < 100) && (t < 100)) {
+      console.log([ "Both are here" ]);
+      return [ ...[ 255, 255, 255 ], t];
+    }
   }
 }
+
+//   reply(){
+//     var r = Math.random();
+//     var i = parseInt(r*255);
+//     return [...[ i, 25, 100 ], 100]
+//   }
+// }
 
 // export default class Alice {
 //   constructor() {
