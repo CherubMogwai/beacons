61425cebe4718566c2f9e843b3ce46fd5f9463d1
Melany Nugent <melnugent@mac.com>
2020-08-27 20:36:21 -0700

    ugly colour

--- models/beacon_base.js
+++ models/beacon_base.js
@@ -56,7 +56,7 @@ export default class Beacon {
       return [ ...[ 0, 0, 255 ], 100];
     } else if (total < 25) {
       console.log([ "Less than 25 meters:", total ]);
-      return [ ...[ 0, 255, 255 ], 100];
+      return [ ...[ 74, 65, 42 ], 100];
     }
   }
 }
