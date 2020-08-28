b822a562e3dbf2427a6cd00f9b0c8fff8f5383d9
Melany Nugent <melnugent@mac.com>
2020-08-27 20:39:31 -0700

    yellow

--- models/beacon_base.js
+++ models/beacon_base.js
@@ -56,7 +56,7 @@ export default class Beacon {
       return [ ...[ 0, 0, 255 ], 100];
     } else if (total < 25) {
       console.log([ "Less than 25 meters:", total ]);
-      return [ ...[ 74, 65, 42 ], 100];
+      return [ ...[255, 255, 0 ], 100];
     }
   }
 }
