28c8dea8d86b47229d86f4b5b4e262ec8b29bb35
Melany Nugent <melnugent@mac.com>
2021-10-13 16:25:27 -0300

    remove elder and major

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ import Brett from 'models/brett';
 import Garance from 'models/garance';
 //import Hollis from 'models/hollis';
 import Minor from 'models/minor';
-import Major from 'models/major';
+//import Major from 'models/major';
 import Aemelia from 'models/aemelia';
 //import Younger from 'models/younger';
-import Elder from 'models/elder';
+//import Elder from 'models/elder';
 import Drusilla from 'models/drusilla';
 import Paulina from 'models/paulina';
 import Marcia from 'models/marcia';
@@ -40,10 +40,10 @@ const NAMES = {
   "e00fce68a140f586ed09a7b6": "Garance",
   //"e00fce6863fc99f591485187": "Hollis",
   "e00fce68aef1348e8458a995": "Minor",
-  "e00fce687ffd264a57a3d47f": "Major",
+  //"e00fce687ffd264a57a3d47f": "Major",
   "e00fce68b49dabcf8d2d2d3f": "Aemelia",
   //"e00fce688f6eee5263cf01e9": "Younger",
-  "e00fce68d9f6cc82af63e8cd": "Elder",
+  //"e00fce68d9f6cc82af63e8cd": "Elder",
   //"e00fce686912e2ca35537271": "Drusilla",
   "e00fce68738521303818cf4c": "Paulina",
   "e00fce68e5d4f33e964ab215": "Marcia",
@@ -72,10 +72,10 @@ const Beacons = {
   "Garance": new BeaconBase('Garance'),
   //"Hollis": new BeaconBase('Hollis'),
   "Minor": new BeaconBase('Minor'),
-  "Major": new BeaconBase('Major'),
+  //"Major": new BeaconBase('Major'),
   "Aemelia": new BeaconBase('Aemelia'),
   //"Younger": new BeaconBase('Younger'),
-  "Elder": new BeaconBase('Elder'),
+  //"Elder": new BeaconBase('Elder'),
   //"Drusilla": new BeaconBase('Drusilla'),
   "Paulina": new BeaconBase('Paulina'),
   "Marcia": new BeaconBase('Marcia'),
